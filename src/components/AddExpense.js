c062d74e415c642f5318360771230b3d59568ec2
mdlulim <mduduzmdluli@gmail.com>
2020-09-16 17:55:32 +0200

    show total expences

--- src/components/AddExpense.js
+++ src/components/AddExpense.js
@@ -1,48 +1,9 @@
-import React, {useState, useContext} from 'react';
-import PropTypes from 'prop-types';
+import React, {useState, useContext} from 'react'
 import { GlobalContext } from '../context/GlobalState';
-import NumberFormat from 'react-number-format';
-import { makeStyles } from '@material-ui/core/styles';
-import TextField from '@material-ui/core/TextField';
-import Input from '@material-ui/core/Input';
-import InputLabel from '@material-ui/core/InputLabel';
-import FormControl from '@material-ui/core/FormControl';
-
-const useStyles = makeStyles((theme) => ({ root: { '& > *': { margin: theme.spacing(1),},},}));
-
-function NumberFormatCustom(props) {
-  const { inputRef, onChange, ...other } = props;
-
-  return (
-    <NumberFormat
-      {...other}
-      getInputRef={inputRef}
-      onValueChange={(values) => {
-        onChange({
-          target: {
-            name: props.name,
-            value: values.value,
-          },
-        });
-      }}
-      thousandSeparator
-      isNumericString
-      prefix="R"
-    />
-  );
-}
-
-NumberFormatCustom.propTypes = {
-  inputRef: PropTypes.func.isRequired,
-  name: PropTypes.string.isRequired,
-  onChange: PropTypes.func.isRequired,
-};
 
 export const AddExpense = () => {
   const [text, setText] = useState('');
   const [amount, setAmount] = useState(0);
-  const classes = useStyles();
-  const [values, setValues] = React.useState({ textmask: '(1  )    -    ', numberformat: '00',});
 
   const { addExpense } = useContext(GlobalContext);
 
@@ -56,13 +17,6 @@ export const AddExpense = () => {
     }
     addExpense(newExpense);
   }
-
-  const handleChange = (event) => {
-    setValues({
-      ...values,
-      [event.target.name]: event.target.value,
-    });
-  };
 
   return (
     <>
@@ -78,11 +32,6 @@ export const AddExpense = () => {
             (negative - expense, positive - income)</label
           >
           <input type="number" value={amount} onChange={(e) => setAmount(e.target.value)} placeholder="Enter amount..." />
-          <TextField label="react-number-format" value={values.numberformat}
-            onChange={handleChange}
-            name="numberformat" id="formatted-numberformat-input"
-            InputProps={{ inputComponent: NumberFormatCustom,}}
-          />
         </div>
         <button className="btn">Add Expense</button>
       </form>
